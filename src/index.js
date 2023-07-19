e267c2541244641b2079e72053dbc528aa6bc04e
Christopher Iza <cdiza5@espe.edu.ec>
2023-07-18 21:37:36 -0500

    Comproboación de Conexión a DB Modificación ReadMe

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 import app from './app.js'
+import {db} from './db.js'
 
+db.connect((err) => {
+    if (err) {
+        console.error("Error en la conexión a la base de datos, error numero:", err);
+        return;
+    }
+    console.log("Conexión exitosa a la base de datos!");
+});
+app.get("/", (req,res)=>{
+    res.json("Conexion al BackEnd")
+})
 app.listen(3000)
 console.log('Servidor en el purerto ',3000)
