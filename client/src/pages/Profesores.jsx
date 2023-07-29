855a39d7b260d5649fbf0d890d164f4b6ed9fdcf
Mila <carivera14@espe.edu.ec>
2023-07-29 16:54:16 -0500

    Componente completo

--- client/src/pages/Profesores.jsx
+++ client/src/pages/Profesores.jsx
@@ -1,116 +1,106 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import SwipeableViews from 'react-swipeable-views';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
-import ArchivosP from '../components/ArchivosP';
+import React, { useState } from 'react';
+import { FormControl, InputLabel, MenuItem, Select, Button, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import '../styles/Profesores.css'; // Import the CSS file
+import { makeStyles } from '@mui/styles';
 
-const user = {
-    name: 'Juan Perez',
-    email: 'algo@espe.edu.ec',
-};
 
-const roles = ['admin', 'user', 'guest'];
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1a237e', // Dark primary color
+    },
+    secondary: {
+      main: '#d32f2f', // Dark secondary color
+    },
+    success: {
+      main: '#00bfa5', // Green color
+    },
+  },
+  typography: {
+    fontFamily: 'YourCoolFont, sans-serif', // Replace 'YourCoolFont' with your preferred cool font
+  },
+});
 
-function TabPanel(props) {
-    const { children, value, index, ...other } = props;
-  
-    return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        aria-labelledby={`full-width-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-    );
-  }
-  
-  TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
+const useStyles = makeStyles((theme) => ({
+  inputLabel: {
+    fontSize: '18px', // Customize the font size
+    color: '#00bfa5', // Customize the label color
+    // Add any other custom styles you want here
+  },
+}));
+
+
+export function Profesores ()  {
+  const [option1, setOption1] = useState('');
+  const [option2, setOption2] = useState('');
+  const [option3, setOption3] = useState('');
+
+  const handleOption1Change = (event) => {
+    setOption1(event.target.value);
   };
-  
-  function a11yProps(index) {
-    return {
-      id: `full-width-tab-${index}`,
-      'aria-controls': `full-width-tabpanel-${index}`,
-    };
-  }
 
+  const handleOption2Change = (event) => {
+    setOption2(event.target.value);
+  };
 
-export function Profesores () {
-    
+  const handleOption3Change = (event) => {
+    setOption3(event.target.value);
+  };
+
+  const classes = useStyles();
+
+  return (
+    <ThemeProvider theme={theme}>
       
-  const theme = useTheme();
-  const [value, setValue] = React.useState(0);
+      <div className="school-system-form-container">
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+      <div className="header">
+          <Typography variant="h4" gutterBottom>
+            Informe de Necesidad Académica
+          </Typography>
+        </div>
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
-  };
+        <div className="form-row">
+          <FormControl fullWidth>
+            <InputLabel className={classes.inputLabel}>Option 1</InputLabel>
+            <Select value={option1} onChange={handleOption1Change}>
+              <MenuItem value="option1_value1">Option 1 Value 1</MenuItem>
+              <MenuItem value="option1_value2">Option 1 Value 2</MenuItem>
+              <MenuItem value="option1_value3">Option 1 Value 3</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
 
-    return (
-  
-      <div>
-  
-        <Box sx={{ bgcolor: '#C8D6C9', width: 1300, marginTop:'8rem', marginLeft:'15%' }}>
-        <AppBar position="static">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor= 'black'
-            textColor="inherit"
-            variant="fullWidth"
-            aria-label="full width tabs example"
-            inkBarStyle={{background: 'black'}}
-            sx={{
-              backgroundColor: '#27662B',}}
-            
-            TabIndicatorProps = {{
-              sx:{
-                backgroundColor: 'Black',}
-  
-            }}
-          >
-            <Tab label="Información Personal" {...a11yProps(0)} sx={{fontFamily: 'poppins, sans-serif'}}/>
-            <Tab label="Información Académica" {...a11yProps(1)} sx={{fontFamily: 'poppins, sans-serif'}}/>
-            <Tab label="..." {...a11yProps(2)} sx={{fontFamily: 'poppins, sans-serif'}}/>
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-          <TabPanel value={value} index={0} dir={theme.direction}>
-            <ArchivosP user={user} roles={roles} />
-          </TabPanel>
-          <TabPanel value={value} index={1} dir={theme.direction}>
-            Item Two
-          </TabPanel>
-          <TabPanel value={value} index={2} dir={theme.direction}>
-            Item Three
-          </TabPanel>
-        </SwipeableViews>
-      </Box>
-  
-  
+        <div className="form-row">
+          <FormControl fullWidth>
+            <InputLabel className={classes.inputLabel}>Option 2</InputLabel>
+            <Select value={option2} onChange={handleOption2Change}>
+              <MenuItem value="option2_value1">Option 2 Value 1</MenuItem>
+              <MenuItem value="option2_value2">Option 2 Value 2</MenuItem>
+              <MenuItem value="option2_value3">Option 2 Value 3</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="form-row">
+          <FormControl fullWidth>
+            <InputLabel className={classes.inputLabel}>Option 3</InputLabel>
+            <Select value={option3} onChange={handleOption3Change}>
+              <MenuItem value="option3_value1">Option 3 Value 1</MenuItem>
+              <MenuItem value="option3_value2">Option 3 Value 2</MenuItem>
+              <MenuItem value="option3_value3">Option 3 Value 3</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+
+        <Button variant="contained" color="success">
+          Send
+        </Button>
       </div>
-    );
+    </ThemeProvider>
+  );
 };
-  
+
+
